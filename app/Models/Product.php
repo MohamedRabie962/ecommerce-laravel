3a533ca33f3080d683d63c50890809058360c998
Mohamed Rabie <mohamedmamdouhrabie110@gmail.com>
2024-11-20 12:48:45 +0200

    commit

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,9 +2,28 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    use HasFactory;
+    protected $fillable = ['name', 'slug', 'description', 'price', 'image',
+        'category_id','is_active', 'brand_id' , 'category_id', 'is_featured','is_stock','on_sale'];
+
+    protected $casts = ['image' => 'array'];
+
+    public function category(){
+        return $this->belongsTo(Category::class);
+    }
+
+    public function brand(){
+        return $this->belongsTo(Brand::class);
+    }
+
+    public function orderitems(){
+        return $this->hasMany(OrderItem::class);
+    }
+
 }
+
